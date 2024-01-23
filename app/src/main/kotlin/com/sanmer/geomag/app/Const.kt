5de56d53233fa039c7849bd38fcb1ce1054aee4f
SanmerDev <sanmer@sanmer.dev>
2024-01-23 18:38:07 +0800

    Update Const

--- app/src/main/kotlin/com/sanmer/geomag/app/Const.kt
+++ app/src/main/kotlin/com/sanmer/geomag/app/Const.kt
@@ -3,6 +3,6 @@ package com.sanmer.geomag.app
 object Const {
     // URL
     const val MY_GITHUB_URL = "https://github.com/SanmerDev"
-    const val TRANSLATE_URL = "https://weblate.sanmer.dev/engage/geomag"
+    const val TRANSLATE_URL = "https://weblate.sanmer.app/engage/geomag"
     const val GITHUB_URL = "https://github.com/SanmerApps/Geomag"
 }
