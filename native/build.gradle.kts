be7f87ed539301fef1f2ef575534a4c34efbaf3d
SanmerDev <sanmer@sanmer.dev>
2024-02-17 23:11:39 +0800

    Add `geomag-jni`

--- native/build.gradle.kts
+++ native/build.gradle.kts
@@ -15,3 +15,23 @@ android {
 dependencies {
     implementation(libs.kotlinx.datetime)
 }
+
+val compileRust = task<Exec>("compileRust") {
+    commandLine(listOf("cargo", "build", "--all-targets", "--release"))
+    workingDir(file("jni"))
+}
+
+task<Copy>("mergeReleaseJniLib") {
+    dependsOn(compileRust)
+    destinationDir = file("src/main/libs")
+
+    listOf(
+        "aarch64-linux-android" to "arm64-v8a",
+        "x86_64-linux-android" to "x86_64"
+    ).forEach { (raw, target) ->
+        from(file("jni/target/${raw}/release")) {
+            include("libgeomag_jni.so")
+            rename("libgeomag_jni.so", "${target}/libgeomag-jni.so")
+        }
+    }
+}
